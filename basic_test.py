Fix determinant gradient bug

basic_test.py
@@ -37,13 +37,23 @@ for i in tqdm(range(2)):
     simulator.Forward(0.01)
     position_np = simulator.position.to_numpy()
     np.save(folder / "{:04d}.npy".format(i + 1), position_np)
+# EPSILON = 2e-3
 # test_pos = ti.Matrix([[0.0, 0.0, 10.0], [1.0, 0.0, 10.0], [0.0, 1.0, 10.0], [0.0, 0.0, 11.0]])
+# test_pos_1 = ti.Matrix([[0.0, 0.0, 10.0], [1.0, 0.0, 10.0], [0.0, 1.0, 10.0], [0.0, 0.0, 11.0]])
+# test_pos_1[0,0] += EPSILON
+# e1 = 0
+# e2 = 0
 
 # @ti.kernel
 # def test():
-#     simulator.ComputeEnergy(test_pos, 0.1)
+#     e1 = simulator.ComputeEnergy(test_pos, 0.1)
+#     e2 = simulator.ComputeEnergy(test_pos_1, 0.1)
+#     print((e2 - e1) / EPSILON)
+#     simulator.ComputeEnergyGradient(test_pos, 0.1)
 
 # test()
+# print(simulator.energy_gradient)
+
 
     
 